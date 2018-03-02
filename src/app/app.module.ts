0a3d70fd7e84d4c0ac4377c37779b67240387019
sekar_neutrinos <gnanasekaran.perumal@neutrinos.co>
2018-03-02 15:47:16 +0530

    angular material added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CoreModule } from './core';
+import { SharedModule } from './shared';
 
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FlexLayoutModule,
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
